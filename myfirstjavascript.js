8f2354940afdd4b35fcb677725587ed58d703f25
Chanyiap <soulkser@hotmail.com>
2017-01-15 15:17:40 +0800

    Add files via upload

---
+++ myfirstjavascript.js
@@ -0,0 +1,17 @@
+document.getElementById('clickMe').onclick = function()
+{
+
+	alert("Just kidding!");
+}
+
+document.getElementById('hello').onmouseover = function(e)
+{
+   document.getElementById('hello').innerHTML="<h1>GoodBye</h1>"
+	
+}
+
+document.getElementById('submit').onclick = function()
+{
+   alert("Hi")
+	
+}
